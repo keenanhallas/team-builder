4b142461e5decde8ecef5d02ca28a80c75e07f1f
Keenan <keenanhallas@gmail.com>
2020-05-11 14:06:29 -0500

    new member form state set up & form inputs linked to changes in state

--- team-builder/src/App.js
+++ team-builder/src/App.js
@@ -1,34 +1,22 @@
 import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import NewMemberForm from "./NewMemberForm";
 
 function App() {
-  const [teamMembers, setTeamMembers] = useState(
-    [
-      {
-        name: "",
-        email: "",
-        role: ""
-      }
-    ]
-  );
+  const [members, setMembers] = useState([{
+    name: "",
+    email: "",
+    role: ""
+  }]);
+
+  const addMember = member => {
+    setMembers([...members, member]);
+  }
 
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <NewMemberForm addMember={addMember}/>
     </div>
   );
 }
